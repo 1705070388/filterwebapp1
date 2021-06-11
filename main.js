e03cfc6f9d7b24419ef5a305fda505908a4d80aa
1705070388 <75253725+1705070388@users.noreply.github.com>
2021-06-11 18:38:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload() { }
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.position(500,200);
+}
+function draw() {
+
+}
+function takeSnapshot() {
+    save("clown-nose-pic.png");
+}
